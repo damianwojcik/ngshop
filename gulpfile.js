abc7ff67cc2ec259afe60747fba43abd61543c1e
khamian <khamian@gmail.com>
2017-02-28 19:02:51 +0100

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var sass = require("gulp-sass");
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task("reload", function() {
+    browserSync.reload();
+});
+
+gulp.task("serve", ["sass"], function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("*.html", ["reload"]);
+    gulp.watch("assets/css/**/*.scss", ["sass"]);
+});
+
+gulp.task("sass", function() {
+    return gulp.src("assets/css/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['> 1%']
+        }))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("default", ["serve"]);
